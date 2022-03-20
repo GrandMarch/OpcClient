47b3e7ed86a3378d45fb7f154c90bd038851693d
何 张 <i.zhanghe@outlook.com>
2022-03-20 13:09:11 +0800

    创建test框架

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+ConsoleKeyInfo cki;
+OpcDaClient.Db.Manager manager = new OpcDaClient.Db.Manager();
+manager.Init();
+manager.Run();
+while (true)
+{
+    Console.Write("Press R to Read Value");
+    Console.Write("Press X to Exit");
+    cki = Console.ReadKey();
+    if (cki.Key == ConsoleKey.R)
+    {
+        
+    }
+    if (cki.Key == ConsoleKey.X)
+    {
+        break;
+    }
+}
+manager.Stop();
