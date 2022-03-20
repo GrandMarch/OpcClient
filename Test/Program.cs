c88be5cac27be0e40912ebca64a8dfbc8110888c
何 张 <i.zhanghe@outlook.com>
2022-03-20 14:30:40 +0800

    op sample

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,18 +6,35 @@ manager.Run();
 string [] tags=manager.GetTagName();
 while (true)
 {
-    Console.Clear();
-    Console.WriteLine("Press R to Read Value");
-    Console.WriteLine("Press X to Exit");
-    Console.WriteLine("Press C to Clear");
+    Console.Write("Press ");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write("R");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(" to Read Value");
+
+    Console.Write("Press ");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write("C");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(" to Clear Screen");
+
+    Console.Write("Press ");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("X");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(" to Exit");
+
+
     cki = Console.ReadKey();
     if (cki.Key == ConsoleKey.R)
     {
         Console.Clear();
+        Console.WriteLine("=====================READ============================");
         foreach (string s in tags)
         {
             Console.WriteLine("{0}\t{1}", s, manager[s]);
         }
+        Console.WriteLine("=====================READ END========================");
     }
     if (cki.Key == ConsoleKey.C)
     {
