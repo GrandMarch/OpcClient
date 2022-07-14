787ff843fa9391c14f2db5783d58b63b9fb6bb75
zhanghe <iamzh@vip.qq.com>
2022-07-14 14:09:53 +0800

    config类移动位置

---
+++ OpcDaClient/Manager/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpcDaClient.Manager
+{
+    internal class Config
+    {
+        public string OpcServer { get; set; } = "";
+        public string Host { get; set; } = "127.0.0.1";
+        public Group[] Groups { get; set; } = new Group[0];
+        public class Item
+        {
+            public string Name { get; set; } = "";
+            public Comn.OpcDataType DataType { get; set; } = Comn.OpcDataType.Int;
+        }
+        public class Group
+        {
+            public string Name { get; set; } = Guid.NewGuid().ToString();
+            public bool Active { get; set; } = true;
+            public float DeadBand { get; set; } = 0.0f;
+            public int UpdateRate { get; set; } = 1000;
+            public Item[] Items { get; set; } = new Item[0];
+        }
+    }
+}
--- OpcDaClient/Manager/Manager.cs
+++ OpcDaClient/Manager/Manager.cs
@@ -116,24 +116,4 @@ namespace OpcDaClient.Manager
             return _db.Keys.ToArray();
         }
     }
-
-    public class Config
-    {
-        public string OpcServer { get; set; } = "";
-        public string Host { get; set; } = "127.0.0.1";
-        public Group[] Groups { get; set; } =new Group[0];
-        public class Item
-        {
-            public string Name { get; set; } = "";
-            public Comn.OpcDataType DataType { get; set; } = Comn.OpcDataType.Int;
-        }
-        public class Group
-        {
-            public string Name { get; set; }= Guid.NewGuid().ToString();
-            public bool Active { get; set; } = true;
-            public float DeadBand { get; set; } = 0.0f;
-            public int UpdateRate { get; set; } = 1000;
-            public Item[] Items { get; set; }=new Item[0];
-        }
-    }
 }
