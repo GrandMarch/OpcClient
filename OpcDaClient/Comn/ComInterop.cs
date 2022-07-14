97b1a64a4bffe45180ca99d2cc9f052026bcf6f2
zhanghe <iamzh@vip.qq.com>
2022-07-14 12:58:26 +0800

    增加opcDiscovery

---
+++ OpcDaClient/Comn/ComInterop.cs
@@ -0,0 +1,158 @@
+﻿using OpcDaClient.Da;
+using OpcRcw.Comn;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpcDaClient.Comn
+{
+    internal class ComInterop
+    {
+        private static readonly Guid IID_IUnknown = new Guid("00000000-0000-0000-C000-000000000046");
+        private struct COSERVERINFO
+        {
+            public uint dwReserved1;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string pwszName;
+            public IntPtr pAuthInfo;
+            public uint dwReserved2;
+        };
+        private struct MULTI_QI
+        {
+            public IntPtr iid;
+            [MarshalAs(UnmanagedType.IUnknown)]
+            public object? pItf;
+            public uint hr;
+        }
+
+        [DllImport("ole32.dll")]
+        private static extern void CoCreateInstanceEx(ref Guid clsid,
+                                                    [MarshalAs(UnmanagedType.IUnknown)] object punkOuter,
+                                                    uint dwClsCtx,
+                                                    [In] ref COSERVERINFO pServerInfo,
+                                                    uint dwCount,
+                                                    [In, Out] MULTI_QI[] pResults);
+        /// <summary>
+        /// Creates an instance of a COM server.
+        /// </summary>
+        public static object? CreateInstance(Guid clsid, string hostName/*, NetworkCredential credential*/)
+        {
+            COSERVERINFO coserverInfo = new();
+            coserverInfo.pwszName = hostName;
+            coserverInfo.pAuthInfo = IntPtr.Zero;
+            coserverInfo.dwReserved1 = 0;
+            coserverInfo.dwReserved2 = 0;
+
+            GCHandle hIID = GCHandle.Alloc(IID_IUnknown, GCHandleType.Pinned);
+
+            MULTI_QI[] results = new MULTI_QI[1];
+
+            results[0].iid = hIID.AddrOfPinnedObject();
+            results[0].pItf = null;
+            results[0].hr = 0;
+
+            try
+            {
+                // check whether connecting locally or remotely.
+                uint clsctx = 0x01 | 0x04;
+
+                if (hostName != null && hostName.Length > 0 && hostName != "localhost")
+                {
+                    clsctx = 0x04 | 0x10;
+                }
+
+                // create an instance.
+#pragma warning disable CS8625 // 无法将 null 字面量转换为非 null 的引用类型。
+                CoCreateInstanceEx(
+                    ref clsid,
+                    null,
+                    clsctx,
+                    ref coserverInfo,
+                    1,
+                    results);
+#pragma warning restore CS8625 // 无法将 null 字面量转换为非 null 的引用类型。
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (hIID.IsAllocated) hIID.Free();
+            }
+
+            if (results[0].hr != 0)
+            {
+                throw new ExternalException("CoCreateInstanceEx: " + (int)results[0].hr);
+            }
+            return results[0].pItf;
+        }
+        public static void RealseComServer(object m_server)
+        {
+            if (m_server != null && m_server.GetType().IsCOMObject)
+            {
+#pragma warning disable CA1416 // 验证平台兼容性
+                Marshal.ReleaseComObject(m_server);
+#pragma warning restore CA1416 // 验证平台兼容性
+            }
+        }
+
+        /// <summary>
+        /// Reads the guids from the enumerator.
+        /// </summary>
+        public static Guid[] ReadClasses(IOPCEnumGUID enumerator)
+        {
+            List<Guid> guids = new List<Guid>();
+
+            int fetched = 0;
+            Guid[] buffer = new Guid[10];
+
+            do
+            {
+                try
+                {
+                    IntPtr pGuids = Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(Guid)) * buffer.Length);
+
+                    try
+                    {
+                        enumerator.Next(buffer.Length, pGuids, out fetched);
+
+                        if (fetched > 0)
+                        {
+                            IntPtr pos = pGuids;
+
+                            for (int ii = 0; ii < fetched; ii++)
+                            {
+                                object? o = Marshal.PtrToStructure(pos, typeof(Guid));
+                                if (o != null)
+                                {
+
+                                    buffer[ii] = (Guid)o;
+                                }
+                                //pos = (IntPtr)(pos.ToInt64() + Marshal.SizeOf(typeof(Guid)));
+                                pos = IntPtr.Add(pos, Marshal.SizeOf(typeof(Guid)));// (IntPtr)(pos.ToInt64() + Marshal.SizeOf(typeof(Guid)));
+                                guids.Add(buffer[ii]);
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        Marshal.FreeCoTaskMem(pGuids);
+                    }
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+            while (fetched > 0);
+
+            return guids.ToArray();
+        }
+    }
+}
---
+++ OpcDaClient/Discovery/OpcDiscovery.cs
@@ -0,0 +1,142 @@
+﻿using OpcRcw.Comn;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpcDaClient.Discovery
+{
+    internal class OpcDiscovery
+    {
+        /// <summary>
+        /// The CLSID for the OPC Server Enumerator.
+        /// </summary>
+        private static readonly Guid OPCEnumCLSID = new("13486D51-4821-11D2-A494-3CB306C10000");
+        private static readonly Guid CATID_OPC_DA10 = new("63D5F430-CFE4-11d1-B2C8-0060083BA1FB");
+        private static readonly Guid CATID_OPC_DA20 = new("63D5F432-CFE4-11d1-B2C8-0060083BA1FB");
+        private static readonly Guid CATID_OPC_DA30 = new("CC603642-66D7-48f1-B69A-B625E73652D7");
+        private IOPCServerList2? m_server = null;
+        private IOPCServerList? oPCServerList = null;
+        private string host = "";
+
+        public OpcDiscovery()
+        {
+
+        }
+        public ServerInfo? GetOpcServer(string serverName, string host)
+        {
+            this.host = host;
+            ServerInfo? result = null;
+            Guid id = typeof(IOPCServerList2).GUID;
+            object? o_Server = Comn.ComInterop.CreateInstance(OPCEnumCLSID, host);
+            if (o_Server == null)
+                throw new ExternalException("GetOpcServerCLSID: CreateInstance Failed");
+            m_server = (IOPCServerList2)o_Server;
+            oPCServerList = (IOPCServerList)o_Server;
+            try
+            {
+                // 目前只关心opc da2.0
+                Guid catid = CATID_OPC_DA20;
+
+                // get list of servers in the specified specification.
+                IOPCEnumGUID? enumerator = null;
+
+#pragma warning disable CS8625 // 无法将 null 字面量转换为非 null 的引用类型。
+                m_server.EnumClassesOfCategories(
+                    1,
+                    new Guid[] { catid },
+                    0,
+                    null,
+                    out enumerator);
+#pragma warning restore CS8625 // 无法将 null 字面量转换为非 null 的引用类型。
+
+                // read clsids.
+                Guid[] clsids = Comn.ComInterop.ReadClasses(enumerator);
+
+                // release enumerator object.	
+                Comn.ComInterop.RealseComServer(enumerator);
+                enumerator = null;
+
+                // fetch class descriptions.
+                ServerInfo[] servers = GetServerDetails(clsids.ToArray());
+
+                for (int i = 0; i < servers.Length; i++)
+                {
+                    if (servers[i].ProgID.ToLower() == serverName.ToLower() || servers[i].VerIndProgID.ToLower() == serverName.ToLower())
+                    {
+                        result = servers[i];
+                        break;
+                    }
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                return result;
+            }
+            finally
+            {
+                // free the server.
+                Comn.ComInterop.RealseComServer(m_server);
+                m_server = null;
+            }
+        }
+        private ServerInfo[] GetServerDetails(Guid[] clsids)
+        {
+            ArrayList servers = new ArrayList();
+            for (int i = 0; i < clsids.Length; i++)
+            {
+                Guid clsid = clsids[i];
+                try
+                {
+                    string? progID = null;
+                    string? description = null;
+                    string? verIndProgID = null;
+                    ServerInfo server1 = new();
+
+                    server1.Host = host;
+                    server1.CLSID = clsid;
+
+                    m_server?.GetClassDetails(
+                        ref clsid,
+                        out progID,
+                        out description,
+                        out verIndProgID);
+                    if (verIndProgID != null)
+                    {
+                        server1.VerIndProgID = verIndProgID;
+                    }
+                    else if (progID != null)
+                    {
+                        server1.ProgID = progID;
+                    }
+                    if (description != null)
+                    {
+                        server1.Description = description;
+                    }
+                    servers.Add(server1);
+                }
+                catch (Exception)
+                {
+                    ;
+                }
+                finally
+                {
+
+                }
+            }
+            return (ServerInfo[])servers.ToArray(typeof(ServerInfo));
+        }
+    }
+    internal class ServerInfo
+    {
+        public string Description { get; set; } = string.Empty;
+        public string VerIndProgID { get; set; } = string.Empty;
+        public string ProgID { get; set; } = string.Empty;
+        public Guid CLSID { get; set; }
+        public string Host { get; set; } = string.Empty;
+    }
+}
